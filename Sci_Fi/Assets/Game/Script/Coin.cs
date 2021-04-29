69e9ba258972e27d4749786a286c363ec3f70c21
Prithviraj Mahamulkar <prithvirajmahamulkar100@gmail.com>
2021-04-29 15:01:20 +0530

    SciFi Game

---
+++ Sci_Fi/Assets/Game/Script/Coin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _coinPickUp;                                                                 //play coin sound
+
+    //check for collision
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")                                                              //check if player
+        {
+            if (Input.GetKeyDown(KeyCode.E))                                                  //check for e key press
+            {
+                Player player = other.GetComponent<Player>();
+                if (player != null)                                                           //give player the coin
+                {
+                    player.hasCoin = true;
+                    AudioSource.PlayClipAtPoint(_coinPickUp, Camera.main.transform.position, 1f);
+                    UIManager uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+                    if (uiManager != null)
+                    {
+                        uiManager.CollectedCoin();
+                    }
+
+                    Destroy(this.gameObject);                                               //destroy the coin
+                }
+            }
+        }
+    }
+    
+    
+
+    
+    
+}
---
+++ Sci_Fi/Assets/Game/Script/SharkShop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkShop : MonoBehaviour
+{
+    //Check for collision
+    //Check if player
+    //check for  E key
+    //check if player has coin
+    //remove coin from player
+    //update the inventory disply
+    //play win sound
+    //Debug Get out of here!
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player player = other.GetComponent<Player>();
+                if (player != null)
+                {
+                    if (player.hasCoin == true)
+                    {
+                        player.hasCoin = false;
+                        UIManager uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+                        if (uiManager != null)
+                        {
+                            uiManager.RemoveCoin();
+                        }
+
+                        AudioSource audio = GetComponent<AudioSource>();
+                        audio.Play();
+                        player.EnableWeapons();
+                    }
+                    else
+                    {
+                        Debug.Log("Get out of here!");
+                    }
+                }
+            }
+        }
+    }
+}
